3224b34b212e58695060d4c2b4ab20f390490032
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-16 10:12:44 -0600

    created car model

---
+++ Models/Car.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace gregslist_csharp.Models;
+
+public class Car
+{
+  public int Id { get; set; }
+
+  [MinLength(3), MaxLength(50)]
+  public string Make { get; set; }
+  [MinLength(1), MaxLength(100)]
+  public string Model { get; set; }
+  [Range(1886, 2025)]
+  public uint Year { get; set; }
+  [Range(0, 1000000)]
+  public uint Price { get; set; }
+  [MaxLength(500)]
+  public string ImgUrl { get; set; }
+  [MaxLength(500)]
+  public string Description { get; set; }
+  public string EngineType { get; set; }
+  [MaxLength(50)]
+  public string Color { get; set; }
+  [Range(0, 1000000)]
+  public uint Mileage { get; set; }
+  // NOTE defaults to true if not present in object being cast into this class
+  public bool HasCleanTitle { get; set; } = true;
+  public string CreatorId { get; set; }
+}
